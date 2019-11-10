a185c982bc1786855d2422447f43b7085ff3f81a
ashkank <ashkank@umich.edu>
2019-11-10 14:24:00 -0500

    Adding the non-square matrix implementation

--- src/TCM_nonsquare_adjacency_matrix.py
+++ src/TCM_nonsquare_adjacency_matrix.py
@@ -1,10 +1,11 @@
 from hash import hash
 
 
-class TCMAdjacencyMatrix:
-    def __init__(self, order, d):
+class TCMANonSquaredjacencyMatrix:
+    def __init__(self, w, l, d):
         self.matrices = [0] * d
-        self.order = order
+        self.w = w
+        self.l = l
         self.d = d
 
     def constructGraph(self, input_graph):
@@ -15,11 +16,11 @@ class TCMAdjacencyMatrix:
             self.matrices[i] = {}
             matrix = self.matrices[i]
             for node in input_graph:
-                hashed_node = hash(node, self.order, i)
+                hashed_node = hash(node, self.w, i)
                 if hashed_node not in matrix:
                     matrix[hashed_node] = {}
                 for adjacent_node in input_graph[node]:
-                    hashed_adjacent_node = hash(adjacent_node, self.order, i)
+                    hashed_adjacent_node = hash(adjacent_node, self.l, i)
                     if hashed_adjacent_node not in matrix[hashed_node]:
                         matrix[hashed_node][hashed_adjacent_node] = 0
                     matrix[hashed_node][hashed_adjacent_node] += input_graph[node][adjacent_node]
